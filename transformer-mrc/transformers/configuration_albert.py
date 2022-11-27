902a952ca8ed5e5086be4fea503336bb65b859a9
Do Quang Minh <91525707+DoQuangMinh55555@users.noreply.github.com>
2022-11-28 02:00:58 +0700

    Update configuration_albert.py

--- transformer-mrc/transformers/configuration_albert.py
+++ transformer-mrc/transformers/configuration_albert.py
@@ -18,10 +18,10 @@
 from .configuration_utils import PretrainedConfig
 
 ALBERT_PRETRAINED_CONFIG_ARCHIVE_MAP = {
-    'albert-base-v1': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-base-config.json",
-    'albert-large-v1': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-config.json",
-    'albert-xlarge-v1': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-xlarge-config.json",
-    'albert-xxlarge-v1': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-xxlarge-config.json",
+    'albert-base-v1': "https://huggingface.co/albert-base-v1/blob/main/config.json",
+    'albert-large-v1': "https://huggingface.co/albert-large-v1/blob/main/config.json",
+    'albert-xlarge-v1': "https://huggingface.co/albert-xlarge-v1/blob/main/config.json",
+    'albert-xxlarge-v1': "https://huggingface.co/albert-xxlarge-v1/blob/main/config.json",
     'albert-base-v2': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-base-v2-config.json",
     'albert-large-v2': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-large-v2-config.json",
     'albert-xlarge-v2': "https://s3.amazonaws.com/models.huggingface.co/bert/albert-xlarge-v2-config.json",
